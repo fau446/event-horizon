6d12e3a7ed2c08f5d082311d4052a7f1235ce4eb
Franklin <Franklinau2@gmail.com>
2024-08-13 10:42:20 -0700

    Add styling to delete confirmation window

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,19 +1,40 @@
+import styles from "../styles/DeleteConfirmation.module.css";
+
 function DeleteConfirmation({ deleteAction, setDisplayConfirmationWindow }) {
   return (
-    <div>
-      <p>Are you sure you want to delete this?</p>
-      <div>
-        <button
-          onClick={() => setDisplayConfirmationWindow(false)}
-          type="button"
-        >
-          Cancel
-        </button>
-        <button onClick={deleteAction} type="button">
-          Delete
-        </button>
+    <>
+      <div className={styles.modal}>
+        <div className={styles.top}>
+          <h2>Delete?</h2>
+          <p
+            className={styles.close}
+            onClick={() => setDisplayConfirmationWindow(false)}
+          >
+            X
+          </p>
+        </div>
+        <p className={styles.message}>Are you sure you want to delete this?</p>
+        <div className={styles.buttons}>
+          <button
+            onClick={() => setDisplayConfirmationWindow(false)}
+            type="button"
+          >
+            Cancel
+          </button>
+          <button
+            className={styles.deleteBtn}
+            onClick={deleteAction}
+            type="button"
+          >
+            Delete
+          </button>
+        </div>
       </div>
-    </div>
+      <div
+        className={styles.overlay}
+        onClick={() => setDisplayConfirmationWindow(false)}
+      ></div>
+    </>
   );
 }
 
